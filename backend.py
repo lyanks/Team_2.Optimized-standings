6148d43152c2178849308eca4a12f307330a726a
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2025-12-01 11:33:11 +0200

    random table

--- backend.py
+++ backend.py
@@ -1,3 +1,6 @@
+import random
+
+
 def single_cycle():
     pass
 
@@ -15,11 +18,29 @@ def read_income(path: str) -> dict:
 
 
 def ranking_table(losers: dict) -> dict:
-    average_point = 1/len(losers)
+    average_point = 1 / len(losers)
     start_leaderboard = {}
     for name in losers:
-            start_leaderboard[name] = average_point
-
+        start_leaderboard[name] = average_point
 
     for i in range(100):
         loos_name = losers
+
+
+def generate_random_table() -> str | None:
+    try:
+        table_length = int(input())
+    except ValueError:
+        return 'Invalid input, write a number'
+    with open('new_table.csv', 'w', encoding='utf-8') as table:
+        table.writelines('winner,looser')
+        for _ in range(table_length):
+            team1, team2 = get_teams()
+            table.writelines(['\n', team1, ',', team2])
+
+
+def get_teams() -> tuple[str, str]:
+    txt = 'Команда '
+    letters = list(chr(i) for i in range(ord('A'), ord('Z') + 1))
+    t1, t2 = random.sample(letters, 2)
+    return txt + t1, txt + t2
