3023f63b4aa9f17ab7845cff4055eecc39caa6a0
AnnaKravchenk0 <a.kravchenko.pn@ucu.edu.ua>
2025-12-06 12:53:34 +0200

    test message

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ def main():
     subprocess.run(["python", "implementation/visual.py"], check=True)
     subprocess.run(["python", "implementation/compare.py"], check=True)
 
+    subprocess.run(["python", "implementation/compare.py"], check=True)
+
 if __name__ == "__main__":
     main()
